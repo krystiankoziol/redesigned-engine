282f9ca37dc43bf2f025978008631fac03c169bd
DawidGolebiowski <1997david5@gmail.com>
2022-03-23 10:03:35 +0100

    add files

---
+++ dg_first.py
@@ -0,0 +1,33 @@
+print(75*"*")
+import math
+
+a = 10
+b = 12
+
+print("a, b")
+print(a, b)
+
+print("type(a)")
+print(type(a))
+
+c = "Pytong basics"
+
+list_a = [1, 2, "Text", (233.51, 234.94)]
+print(list_a)
+
+print(20*"_")
+
+for i in list_a:
+    print("typ i wartosc")
+    print(type(i))
+    print(i)
+    print(10*"*")
+
+for i in list_a:
+    print("typ:", type(i), "  wartosc:", i)
+
+print(math.sin(10), math.pi)
+
+print(c[3], c[-2], c[3:8])
+print(c[3:8])
+print(/n)
